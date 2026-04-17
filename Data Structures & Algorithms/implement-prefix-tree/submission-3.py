9f16d61a5061af7b70e390d6a03459f93120eba1
Peihan Wang <wang.peiha@northeastern.edu>
2026-04-16 21:03:13 -0700

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        node = self._traverse(word)
+        return node is not None and node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._traverse(prefix) is not None
+    
+    def _traverse(self, s):
+        node = self.root
+        for ch in s:
+            if ch not in node.children:
+                return None
+            node = node.children[ch]
+        return node
+        
